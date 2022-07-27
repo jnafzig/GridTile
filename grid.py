e2beb6f68511b960151f3b6281cf7645cea63a3b
jonathan <jnafziger@gmail.com>
2022-07-27 15:00:55 -0700

    cleanup

--- grid.py
+++ grid.py
@@ -9,15 +9,30 @@ from typing import Optional
 from itertools import chain
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
-plt.rcParams["figure.figsize"] = (15, 15)
+import time
+import logging 
+logging.basicConfig(level=logging.INFO)
 
-   
+logger = logging.getLogger(__name__)
+
+class Timer(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __enter__(self):
+        self.t0 = time.time()
+        logger.info(f'starting {self.text}')
+        return self.t0
+
+    def __exit__(self, type, value, traceback):
+        logger.info(f'{self.text} took {time.time()-self.t0} seconds')
+
 def rot(theta):
-    return jnp.array([
+    return np.array([
         [np.cos(theta), -np.sin(theta)],
         [np.sin(theta), np.cos(theta)]
-    ], dtype=jnp.float64)
-rot90 = rot(jnp.pi/2)
+    ], dtype=np.float64)
+rot90 = rot(np.pi/2)
 
 def intersection(vec1, vec2):
     normal1 = vec1 / jnp.linalg.norm(vec1)
@@ -29,7 +44,8 @@ def intersection(vec1, vec2):
         vec2-vec1
     )
     return x[0] * direction1 + vec1
-get_intersections = jax.vmap(
+
+get_jax_intersections = jax.vmap(
     jax.vmap(
         intersection,
         (0, None),
@@ -38,6 +54,12 @@ get_intersections = jax.vmap(
     (None, 0),
     1
 )
+get_intersections = lambda x, y: np.array(get_jax_intersections(x, y))
+
+def get_basis(N):
+    return np.stack(
+        [rot(2*np.pi * i / N) @ np.array([1, 0]) for i in range(N)]
+    )
 
 def intersection_filter(intersection):
     if np.any(np.isnan(intersection)):
@@ -49,45 +71,69 @@ class Grid:
         self.lines = lines
         self.n = len(lines)
         self.norms = lines / np.linalg.norm(lines, axis=1)[..., np.newaxis]
-        self.directions = (rot90 @ self.norms[..., jnp.newaxis]).squeeze()
+        self.directions = (rot90 @ self.norms[..., np.newaxis]).squeeze()
 
-        print('finding intersections')
-        self.intersections = get_intersections(lines, lines)
 
-        print('creating rhombuses')
-        self.rhombuses = {}
-        for i in range(self.n):
-            for j in range(i+1, self.n):
-                self.rhombuses[frozenset((i,j))] = Rhomb(
-                    id=f'{i},{j}',
-                    sides={
-                        j: self.norms[i],
-                        i: self.norms[j]
-                    },
-                    directions={
-                        j: self.directions[j],
-                        i: self.directions[i]
-                    }
-                )
+        with Timer('finding intersections'):
+            self.intersections = get_intersections(lines, lines)
 
-        print('sorting intersections')
-        self.orderings = np.argsort(
-            (self.directions[:, np.newaxis, :] * self.intersections).sum(axis=2),
-            axis=1
-        )
-        print('connecting rhombuses')
-        for i in range(self.n):
-            print(i)
-            ordering = self.orderings[i].tolist()
-            ordering = [x for x in ordering 
-                if x != i and intersection_filter(self.intersections[i][x])
-            ]
-            for j1, j2 in zip(ordering[:-1], ordering[1:]):
-                rhomb1 = self.rhombuses[frozenset((i, j1))]
-                rhomb2 = self.rhombuses[frozenset((i, j2))]
-                rhomb1.up_neighbors[i] = rhomb2
-                rhomb2.down_neighbors[i] = rhomb1
+        with Timer('creating rhombuses'):
+            self.rhombuses = {}
+            first_intersection = None
+            for i in range(self.n):
+                for j in range(i+1, self.n):
+                    self.rhombuses[frozenset((i,j))] = Rhomb(
+                        id=f'{i},{j}',
+                        sides={
+                            j: self.norms[i],
+                            i: self.norms[j]
+                        },
+                        directions={
+                            j: self.directions[j],
+                            i: self.directions[i]
+                        }
+                    )
+                    if first_intersection is None and intersection_filter(self.intersections[i, j]):
+                        first_intersection = (i, j)
 
+        with Timer('sorting intersections'):
+            self.orderings = np.argsort(
+                (self.directions[:, np.newaxis, :] * self.intersections).sum(axis=2),
+                axis=1
+            )
+
+        with Timer('connecting rhombuses'):
+            for i in range(self.n):
+                ordering = self.orderings[i].tolist()
+                ordering = [x for x in ordering 
+                    if x != i and intersection_filter(self.intersections[i][x])
+                ]
+                for j1, j2 in zip(ordering[:-1], ordering[1:]):
+                    rhomb1 = self.rhombuses[frozenset((i, j1))]
+                    rhomb2 = self.rhombuses[frozenset((i, j2))]
+                    rhomb1.up_neighbors[i] = rhomb2
+                    rhomb2.down_neighbors[i] = rhomb1
+
+        with Timer('positioning rhombuses'):
+            positioned_rhomb = frozenset(first_intersection)
+            self.rhombuses[positioned_rhomb].position = np.zeros(2)
+            self.rhombuses[positioned_rhomb].position_neighbors(cascade=True)
+
+    @classmethod
+    def regular_grid(cls, num_directions, num_lines, offsets=None):
+        basis = get_basis(num_directions)
+        if offsets is None:
+            offsets = np.random.random(num_directions)
+        grid_range = np.arange(num_lines) - num_lines // 2
+
+        lines = np.zeros((len(basis), len(grid_range), 2), dtype=float)
+
+        for i, (vector, offset) in enumerate(zip(basis, offsets)):
+            for j, k in enumerate(grid_range):
+                lines[i, j] = vector * (k + offset)
+
+        lines = lines.reshape(-1,2)
+        return cls(lines)
 
     def plot_lines(self, ax, debug=False):
         for line, norm, direction in zip(self.lines, self.norms, self.directions):
@@ -103,8 +149,8 @@ class Grid:
                 if not intersection_filter(self.intersections[i,j]):
                     continue
                 if debug:
-                    ax.plot(*intersections[i,j,:], 'o')
-                    ax.annotate(f'{i},{j}', intersections[i,j,:])
+                    ax.plot(*self.intersections[i,j,:], 'o')
+                    ax.annotate(f'{i},{j}', self.intersections[i,j,:])
                 else:
                     pass
                     #ax.plot(*intersections[i,j,:], '.')
@@ -123,42 +169,6 @@ class Grid:
                     #ax.arrow(*(rhomb.position), *rhomb.directions[key], width=.02, color='k')
         ax.add_collection(p)
     
-def get_basis(N):
-    return np.stack(
-        [rot(2*np.pi * i / N) @ np.array([1, 0]) for i in range(N)]
-    )
-
-N = 7
-basis = get_basis(N)
-offsets = np.random.random(N)
-grid_range = range(-10,11)
-
-lines = np.zeros((len(basis), len(grid_range), 2), dtype=float)
-
-for i, (vector, offset) in enumerate(zip(basis, offsets)):
-    for j, k in enumerate(grid_range):
-        lines[i, j] = vector * (k + offset)
-
-lines = lines.reshape(-1,2)
-positioned_rhomb = frozenset((0,30))
-#lines = np.random.random((20,2)) * 2 - 1
-
-print(repr(lines))
-
-#lines = np.array([
-#    [-0.80486042, -0.6139645 ],
-#    [-0.38972012, -0.23409338],
-#    [ 0.4669395,  -0.52421947],]
-#)
-#lines = np.array([
-#    [-0.82241089, -0.24806271],
-#    [-0.40205683, -0.79968747],
-#    [ 0.64082981, -0.64682302]
-#])
-#
-#lines = np.random.random((10, 2))*2-1
-#positioned_rhomb = frozenset((0,1))
-
 
 @dataclass
 class Rhomb:
@@ -173,7 +183,6 @@ class Rhomb:
         stack = [self]
         while stack:
             self = stack.pop()
-            #print('positioning', self.id)
             newly_positioned = []
             for key, neighbor in self.up_neighbors.items():
                 if neighbor.position is not None:
@@ -182,7 +191,6 @@ class Rhomb:
                 vec2 = neighbor.sides[key] * np.sign(neighbor.sides[key] @ neighbor.directions[key])
                 neighbor.position = self.position + (vec1 + vec2) / 2
                 newly_positioned.append(neighbor)
-                #print(neighbor.id, 'up')
             for key, neighbor in self.down_neighbors.items():
                 if neighbor.position is not None:
                     continue
@@ -190,7 +198,6 @@ class Rhomb:
                 vec2 = neighbor.sides[key] * np.sign(neighbor.sides[key] @ neighbor.directions[key])
                 neighbor.position = self.position - (vec1 + vec2) / 2
                 newly_positioned.append(neighbor)
-                #print(neighbor.id, 'down')
             if cascade:
                 stack.extend(newly_positioned)
 
@@ -217,22 +224,19 @@ class Rhomb:
         ]) / 2 + self.position
         return Polygon(rhombus, closed=True)
 
-grid = Grid(lines)
 
-grid.rhombuses[positioned_rhomb].position = np.zeros(2)
-grid.rhombuses[positioned_rhomb].position_neighbors(cascade=True)
+if __name__ == '__main__':
+    grid = Grid.regular_grid(5, 11)
 
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), gridspec_kw={'wspace':0, 'hspace':0})
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(7, 10), gridspec_kw={'wspace':0, 'hspace':0})
+    with Timer('plotting'):
+        grid.plot_lines(ax1)
+        grid.plot_rhombuses(ax2)
 
-grid.plot_lines(ax1)
-grid.plot_rhombuses(ax2)
+    ax1.axis('square')
+    ax1.axis('off')
 
-ax1.axis('square')
-ax1.axis('off')
-
-ax2.axis('square')
-ax2.axis('off')
-#plt.savefig('/home/jonathan/Desktop/step5.png', dpi=300)
-plt.show()
-#    
+    ax2.axis('square')
+    ax2.axis('off')
+    plt.show()
